47bd9806cfc35c27fb2f82faa77b167d2d85d228
deepikaarcot18-png <deepikaarcot18@gmail.com>
2026-06-11 16:16:30 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
- theme: {
-  extend: {
-    fontFamily: {
-      serif: ["Times New Roman", "Times", "serif"],
-    },
+  theme: {
+    extend: {},
   },
-},
   plugins: [],
 };
